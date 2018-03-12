80a2da7a95203837ba0a775db57d11ec0b6fa4c9
wade-sheedy <30946766+wade-sheedy@users.noreply.github.com>
2018-03-12 21:50:02 +1000

    Add files via upload

---
+++ Is_This_a_String/Is_This_a_String.py
@@ -0,0 +1,8 @@
+#String Checker for menu systems testing to see if the input is a string
+# and returns a simple true or flase value
+def Is_This_A_String(User_String_Input):
+    """just a string testing function for user input validation of a string type"""
+    if User_String_Input == str():
+        return True
+    else:
+        return False
